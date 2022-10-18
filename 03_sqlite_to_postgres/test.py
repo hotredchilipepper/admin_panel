0847f1b5def6c0068737f3c92a8fce79a415a892
hotredchilipepper <belozertsevyura@gmail.com>
2022-10-18 13:24:07 +0700

    commit delete comments

--- 03_sqlite_to_postgres/test.py
+++ 03_sqlite_to_postgres/test.py
@@ -9,9 +9,7 @@ db_path = 'db.sqlite'
 def conn_context(db_path: str):
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
-    yield conn # С конструкцией yield вы познакомитесь в следующем модуле 
-    # Пока воспринимайте её как return, после которого код может продолжить выполняться дальше
-    # Формируем запрос. Внутри execute находится обычный SQL запрос
+    yield conn
 
 
 db_path = 'db.sqlite'
